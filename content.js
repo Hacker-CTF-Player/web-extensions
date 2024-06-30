628b7694d040b05d0089c5379cbc587964787b9c
Hacker <162045216+Hacker-CTF-Player@users.noreply.github.com>
2024-06-30 18:12:00 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,4 @@
+// Find all input fields and set their value to "Hello ROHIT"
+document.querySelectorAll('input[type="text"], input[type="email"], input[type="password"], textarea').forEach(function(input) {
+    input.value = "Hello ROHIT";
+});
